982a93663e94c0f63961be795d732b4f2a066abf
ednavho <ednaho121@gmail.com>
2022-04-14 09:13:54 -0700

    make new file for testing

--- MarkdownLinkCount.java
+++ MarkdownLinkCount.java
@@ -1,16 +1,17 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParse {
+public class MarkdownLinkCount {
 
-    public static ArrayList<String> getLinks(String markdown) {
+
+    public static int getLinkCount(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
@@ -18,17 +19,21 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            //System.out.println(currentIndex);
         }
 
-        return toReturn;
+        int linkCount = 0;
+        for (int i = 0; i < toReturn.size(); ++i) {
+            linkCount += 1;
+        }
+        return linkCount;
     }
 
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        int linkCount = getLinkCount(content);
+	    System.out.println(linkCount);
     }
 }
+
