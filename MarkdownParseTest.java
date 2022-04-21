460771fbe123b489b4dbc513f259aafbcb7caa00
ednavho <ednaho121@gmail.com>
2022-04-21 09:09:41 -0700

    new tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
 
@@ -7,5 +11,28 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1); //before comma is expected result and right is actual
     }
+
+    @Test
+    public void getLinksFromFile1() throws IOException{
+        Path fileName = Path.of(test-file.md);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new ArrayList<>();
+        links.add("https://something.com");
+        links.add("some-thing.html");
+        assertEquals(links, MarkdownParse.getLinks(content));
+    }
+
+    /*@Test
+    public void getLinksFromFile2() throws IOException{
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new ArrayList<>();
+        links.add("https://something.com");
+        links.add("some-thing.html");
+        links.add("");
+        links.add("https://google.com");
+        assertEquals(links, MarkdownParse.getLinks(content));
+    }*/
+    
     
 }
--- test-file2.md
+++ test-file2.md
@@ -2,7 +2,6 @@
 
 [link1](https://something.com)
 [link2](some-thing.html)
-[  ]   () 
+[  ]() 
 
 [link3](https://google.com)
-
