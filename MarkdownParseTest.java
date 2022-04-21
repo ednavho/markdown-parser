1512ce4076ac05253da4875e8ce2c8c3662c00da
ednavho <ednaho121@gmail.com>
2022-04-21 08:48:43 -0700

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+
+    @Test //tells program that this will be a test
+    public void addition() {
+        assertEquals(2, 1 + 1); //before comma is expected result and right is actual
+    }
+    
+}
